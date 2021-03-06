f8e7d71449f12aed0d7578cdb25ac0d50cb870ce
mike <mike@zipcodes-mbp-5.lan>
2021-03-05 21:51:37 -0500

    table is off, everything else looks good

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,57 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        return getRange(0,stop);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start,stop,1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for(int i=start; i<stop; i+=step){
+            range += i;
+        }
+        return range;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String even = "";
+        for(int i = start; i<stop; i++){
+        if (i%2 == 0){
+        even+=i;
     }
+    
+        
+    }
+    return even;
+    }
+
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String odd = "";
+        for(int i = start; i<stop; i++){
+        if (i%2 == 1){
+        odd+=i;
+        } 
     }
+    return odd;
+    }
+
+    
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String expo = "";
+        int calc = 0;
+        for(int i=start; i<=stop; i++){
+            calc = (int)Math.pow(i,exponent);
+            expo += calc;
+            
+    }
+    return expo;
     }
 }
--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,29 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String table = " ";
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5; j++){
+                table += " " + (i * j) + " |";
+            }table +="\n";
+    }return table;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String table = " ";
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                table += " " + (i * j) + " |";
+            }table +="\n";
+    }return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = " ";
+        for(int i = 1; i <= tableSize; i++){
+            for(int j = 1; j <= tableSize; j++){
+                table += " " + (i * j) + " |";
+            }table +="\n";
+    }return table;
     }
 }
